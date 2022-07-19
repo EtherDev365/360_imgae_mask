28f01cafab73a6a4a778f06e21017c22bf6630ab
rockstar753 <williemL@outlook.com>
2022-07-19 15:37:22 -0700

    mask effect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { useEffect } from 'react';
 import './App.css';
 import {create360Image} from './components/Image360'
+import { Cursor } from 'react-creative-cursor';
+import 'react-creative-cursor/dist/styles.css';
 
 function App() {
-  useEffect(() => {
-    create360Image()
-  },[])
+  // useEffect(() => {
+  //   create360Image()
+  // },[])
   return (
     <div className="App">
-      
+      <Cursor isGelly={true} />
+      <div data-cursor-exclusion style={{backgroundColor: '#fff'}} data-cursor-size="80px">
+        <img src='https://reactjs.org/logo-og.png' width={1700}/>
+      </div>
     </div>
   );
 }
