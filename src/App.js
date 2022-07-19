be8666bbc2ce033a905394099246c6012b687e52
rockstar753 <williemL@outlook.com>
2022-07-19 15:47:27 -0700

    fix error 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import {create360Image} from './components/Image360'
 import { Cursor } from 'react-creative-cursor';
 import 'react-creative-cursor/dist/styles.css';
 
@@ -8,7 +7,7 @@ function App() {
     <div className="App">
       <Cursor isGelly={true} />
       <div data-cursor-exclusion style={{backgroundColor: '#fff'}} data-cursor-size="80px">
-        <img src='https://reactjs.org/logo-og.png' width={1700}/>
+        <img src='https://reactjs.org/logo-og.png' width={1700} alt="image error"/>
       </div>
     </div>
   );
