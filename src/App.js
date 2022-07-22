70122579c7beb2283e7e27ae902d938bfe125fe8
rockstar753 <williemL@outlook.com>
2022-07-21 17:32:54 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import './App.css';
-import { Cursor } from 'react-creative-cursor';
-import 'react-creative-cursor/dist/styles.css';
+import "./demo.css";
+import React from 'react'
+//import { Cursor } from './react-creative-cursor/dist';
+import Cursor from "react-special-cursor";
+import './react-creative-cursor/dist/styles.css';
+import background from './assets/images/1.png';
+import BlackBack from './assets/images/black.png';
+
 
 function App() {
+  const [black, setBlack] = React.useState(false)
+  const leaveHandler = () =>{
+    console.log("hello");
+    setBlack(false)
+  }
+  const enterHandler = () =>{
+    console.log("hello");
+    setBlack(true)
+  }
   return (
-    <div className="App">
-      <Cursor isGelly={true} />
-      <div data-cursor-exclusion style={{backgroundColor: '#fff'}} data-cursor-size="80px">
-        <img src='https://reactjs.org/logo-og.png' width={1700} alt="error"/>
+    <Cursor
+      hoverClasses={[
+        { classNameOfTargetElement: "head", classNameOfStyle: "head-hover" },
+        { classNameOfTargetElement: "link", classNameOfStyle: "hover" },
+        { classNameOfTargetElement: "react", classNameOfStyle: "react-hover" },
+        {
+          classNameOfTargetElement: "children",
+          classNameOfStyle: "children-hover",
+        },
+      ]}>
+      <div className="head">
+        <img src={black?background:BlackBack} width="100%" height="100%" alt="error"
+         onMouseLeave={()=>leaveHandler()}
+         onMouseEnter={()=>enterHandler()} />
       </div>
-    </div>
+    </Cursor>
   );
 }
 
